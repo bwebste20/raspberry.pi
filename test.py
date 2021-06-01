f96f9b86beb5d507ed51bbb9733770fd96702991
bwebste20 <bwebste20@charlottesvilleschools.org>
2021-06-01 19:15:37 +0100

    adding test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+print("trying to make an led blink")
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
+
+
+while True: 
+ GPIO.output(8, GPIO.HIGH)
+ sleep(1)
+ GPIO.output(8, GPIO.LOW)
+ sleep(1)
