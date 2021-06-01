f8c5832c08e239777e97407d3098d9bae528eb66
bwebste20 <bwebste20@charlottesvilleschools.org>
2021-06-01 19:23:00 +0100

    getting calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+txt = input("Give me a number: ")
+txt2 = input("give me another number")
+txtInt = int(txt)
+txtint2 = int(txt2)
+prod1 = txtInt+txtint2
+prod2 = txtInt*txtint2
+prod3 = txtInt-txtint2
+prod4 = txtInt/txtint2
+prodStr1 = str(prod1)
+prodStr2 = str(prod2)
+prodStr3 = str(prod3)
+prodStr4 = str(prod4)
+print("Sum:\t\t" + prodStr1)
+print("product:\t\t" + prodStr2)
+print("quotient:\t\t" + prodStr3)
+print("difference:\t\t" + prodStr4)
